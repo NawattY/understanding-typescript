7c6f1681d7dbc6e4b3bb089e1cf19baf8e1d939e
Nawatt Y <golf.enfc40@gmail.com>
2020-05-13 22:15:14 +0700

    delete .history

--- .history/src/app_20200513211305.ts
+++
@@ -1,9 +0,0 @@
-class ProjectInput {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
-
-  constructor() {
-    this.templateElement = document.getElementById('project-input') as HTMLTemplateElement;
-    this.hostElement = <HTMLDivElement>document.getElementById('app');
-  }
-}
