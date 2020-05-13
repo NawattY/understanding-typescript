9ce0562eba049e5a21af5db62233f080c386e8d8
Nawatt Y <golf.enfc40@gmail.com>
2020-05-13 23:45:52 +0700

    validate

--- dist/app.js
+++ dist/app.js
@@ -16,6 +16,52 @@ function autoBind(_, _2, descriptor) {
     };
     return adjDescriptor;
 }
+function validate(validatableInputs) {
+    let isValid = true;
+    let errors = [];
+    for (const index in validatableInputs) {
+        const validatableInput = validatableInputs[index];
+        const inputErrors = [];
+        if (validatableInput.required && validatableInput.value.toString().trim().length === 0) {
+            isValid = false;
+            inputErrors.push(`The ${validatableInput.name} is required.`);
+        }
+        if (validatableInput.numeric && typeof validatableInput.value !== 'number') {
+            isValid = false;
+            inputErrors.push(`The ${validatableInput.name} must be numeric.`);
+        }
+        if (validatableInput.min) {
+            if (typeof validatableInput.value === 'number' && validatableInput.value < validatableInput.min) {
+                isValid = false;
+                inputErrors.push(`The ${validatableInput.name} must greater or equal than ${validatableInput.min}.`);
+            }
+            else if (typeof validatableInput.value === 'string' && validatableInput.value.trim().length < validatableInput.min) {
+                isValid = false;
+                inputErrors.push(`The ${validatableInput.name} length must greater or equal than ${validatableInput.min}.`);
+            }
+        }
+        if (validatableInput.max) {
+            if (typeof validatableInput.value === 'number' && validatableInput.value > validatableInput.max) {
+                isValid = false;
+                inputErrors.push(`The ${validatableInput.name} must less or equal than ${validatableInput.max}.`);
+            }
+            else if (typeof validatableInput.value === 'string' && validatableInput.value.trim().length > validatableInput.max) {
+                isValid = false;
+                inputErrors.push(`The ${validatableInput.name} length must less or equal than ${validatableInput.max}.`);
+            }
+        }
+        if (inputErrors.length > 0) {
+            errors.push({
+                name: validatableInput.name,
+                errors: inputErrors
+            });
+        }
+    }
+    return {
+        isValid: isValid,
+        errors: errors
+    };
+}
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
@@ -29,9 +75,35 @@ class ProjectInput {
         this.configure();
         this.attach();
     }
+    gatherUserInput() {
+        const title = this.titleInputElement.value;
+        const description = this.descriptionInputElement.value;
+        const people = this.peopleInputElement.value;
+        const validator = validate([
+            { name: 'title', value: title, required: true, min: 10 },
+            { name: 'description', value: description, required: true, min: 10 },
+            { name: 'people', value: people, required: true, numeric: true, min: 1 }
+        ]);
+        if (!validator.isValid) {
+            alert('invalid');
+            console.log(validator.errors);
+            return;
+        }
+        return [title, description, parseInt(people)];
+    }
+    clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
     submitHandler(event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, description, people] = userInput;
+            console.log(title, description, people);
+            this.clearInputs();
+        }
     }
     configure() {
         this.element.addEventListener('submit', this.submitHandler);
--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,77 @@ function autoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjDescriptor
 }
 
+interface validatable {
+  name: string
+  value: string | number
+  required?: boolean
+  numeric?: boolean
+  min?: number
+  max?: number
+}
+
+interface validationError {
+  name: string
+  errors: string[]
+}
+
+type validation = {
+  isValid: boolean
+  errors: validationError[]
+}
+
+function validate(validatableInputs: validatable[]): validation {
+  let isValid = true
+  let errors = []
+
+  for (const index in validatableInputs) {
+    const validatableInput = validatableInputs[index]
+    const inputErrors = []
+
+    if (validatableInput.required && validatableInput.value.toString().trim().length === 0) {
+      isValid = false
+      inputErrors.push(`The ${validatableInput.name} is required.`)
+    }
+  
+    if (validatableInput.numeric && typeof validatableInput.value !== 'number') {
+      isValid = false
+      inputErrors.push(`The ${validatableInput.name} must be numeric.`)
+    }
+  
+    if (validatableInput.min) {
+      if (typeof validatableInput.value === 'number' && validatableInput.value < validatableInput.min) {
+        isValid = false
+        inputErrors.push(`The ${validatableInput.name} must greater or equal than ${validatableInput.min}.`)
+      } else if (typeof validatableInput.value === 'string' && validatableInput.value.trim().length < validatableInput.min) {
+        isValid = false
+        inputErrors.push(`The ${validatableInput.name} length must greater or equal than ${validatableInput.min}.`)
+      }
+    }
+  
+    if (validatableInput.max) {
+      if (typeof validatableInput.value === 'number' && validatableInput.value > validatableInput.max) {
+        isValid = false
+        inputErrors.push(`The ${validatableInput.name} must less or equal than ${validatableInput.max}.`)
+      } else if (typeof validatableInput.value === 'string' && validatableInput.value.trim().length > validatableInput.max) {
+        isValid = false
+        inputErrors.push(`The ${validatableInput.name} length must less or equal than ${validatableInput.max}.`)
+      }
+    }
+
+    if (inputErrors.length > 0) {
+      errors.push({
+        name: validatableInput.name,
+        errors: inputErrors
+      })
+    }
+  }
+
+  return {
+    isValid: isValid,
+    errors: errors
+  }
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement
   hostElement: HTMLDivElement
@@ -35,10 +106,42 @@ class ProjectInput {
     this.attach()
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const title = this.titleInputElement.value
+    const description = this.descriptionInputElement.value
+    const people = this.peopleInputElement.value
+
+    const validator = validate([
+      { name: 'title', value: title, required: true, min: 10 },
+      { name: 'description', value: description, required: true, min: 10 },
+      { name: 'people', value: people, required: true, numeric: true, min: 1 }
+    ])
+
+    if (!validator.isValid) {
+      alert('invalid')
+      console.log(validator.errors)
+      return
+    }
+
+    return [title, description, parseInt(people)]
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = ''
+    this.descriptionInputElement.value = ''
+    this.peopleInputElement.value = ''
+  }
+
   @autoBind
   private submitHandler(event: Event) {
     event.preventDefault()
-    console.log(this.titleInputElement.value)
+    const userInput = this.gatherUserInput()
+
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput
+      console.log(title, description, people)
+      this.clearInputs()
+    }
   }
 
   private configure() {
